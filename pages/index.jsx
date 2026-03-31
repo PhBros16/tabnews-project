b4706887f22f2f4b84aeabdc1b55d58d09e84261
PhBros16 <pedrowskcartola@gmail.com>
2026-03-31 19:25:55 +0000

    modifica a frase

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Alô Alô Marciano, aqui quem fala é da Terraaaa</h1>
+    return <h1>Alô Alô Marciano, aqui quem fala é da Terraaaa🤪</h1>
 }
 
 export default Home;
